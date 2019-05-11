dfe904b193212b9ccd9d0b81baeadc40f1af6b5c
TinyWendy <tinyliu@me.com>
2019-05-11 17:32:58 +0800

    Started!

---
+++ NGAFaker/postTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  postTableViewCell.swift
+//  NGAFaker
+//
+//  Created by TinyLiu on 2019/5/11.
+//  Copyright © 2019 TinyLiu. All rights reserved.
+//
+
+import UIKit
+
+class postTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
